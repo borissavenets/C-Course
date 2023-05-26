36832f8e41e185e16cd73594f64264988fcf2557
Borys.Savenets <Borys.Savenets@BSAVENETS-N>
2023-05-26 13:11:03 +0300

    Add project files.

---
+++ ConsoleApp1 - HM1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Transactions;
+
+namespace ConsoleApp1___HM1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+            decimal solaryCompany;
+
+            solaryCompany = Decimal.One;
+            Console.WriteLine(solaryCompany);
+            var test = Console.ReadLine();
+
+            Console.WriteLine(test);
+            Console.WriteLine (test+1);
+
+            const double pi = 3.14;
+            
+            Console.WriteLine("Enter number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+           
+            Console.WriteLine (number); 
+        }
+    }
+}
